1c83a8de05e8c9b40193a4afc4762e31113415c5
Solomon Feldman <solomonrfeldman@gmail.com>
2019-10-17 00:14:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const removeLast = ['Milo', 'Otis', 'Garfield']
 const removeFirst = ['Milo', 'Otis', 'Garfield']
 
 append.push('Odie')
-prepend.shift('Odie')
+prepend.unshift('Odie')
 removeLast.pop()
-removeFirst.unshift()
+removeFirst.shift()
