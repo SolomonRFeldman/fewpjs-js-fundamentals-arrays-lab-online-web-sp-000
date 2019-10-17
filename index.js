d9740be54e616b75722cbf29f7c81dec98ddd39b
Solomon Feldman <solomonrfeldman@gmail.com>
2019-10-17 00:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const append = ['Milo', 'Otis', 'Gerfield'];
-const prepend = ['Milo', 'Otis', 'Gerfield'];
-const removeLast = ['Milo', 'Otis', 'Gerfield'];
-const removeFirst = ['Milo', 'Otis', 'Gerfield'];
+const append = ['Milo', 'Otis', 'Gerfield']
+const prepend = ['Milo', 'Otis', 'Gerfield']
+const removeLast = ['Milo', 'Otis', 'Gerfield']
+const removeFirst = ['Milo', 'Otis', 'Gerfield']
 
-append.push('Odie');
-prepend.unhshift('Odie');
-removeLast.pop();
-removeFirst.shift();
+append.push('Odie')
+prepend.unhshift('Odie')
+removeLast.pop()
+removeFirst.shift()
